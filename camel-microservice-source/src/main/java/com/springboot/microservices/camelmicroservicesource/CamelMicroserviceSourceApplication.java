569607177059479b1a021aa148ca6179f4528d04
codingconquerors <100064563+codingconquerors@users.noreply.github.com>
2022-08-10 14:10:13 -0400

    initial check in for camel

---
+++ camel-microservice-source/src/main/java/com/springboot/microservices/camelmicroservicesource/CamelMicroserviceSourceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.camelmicroservicesource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceSourceApplication.class, args);
+	}
+
+}
---
+++ camel-microservice-source/src/test/java/com/springboot/microservices/camelmicroservicesource/CamelMicroserviceSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.camelmicroservicesource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroserviceSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
