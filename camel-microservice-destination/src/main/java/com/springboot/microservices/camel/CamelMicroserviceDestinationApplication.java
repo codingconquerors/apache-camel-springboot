6d48cc2a8742b39db038947e10adb24ef5deb989
codingconquerors <100064563+codingconquerors@users.noreply.github.com>
2022-08-12 16:24:35 -0400

    Active mq receiver

---
+++ camel-microservice-destination/src/main/java/com/springboot/microservices/camel/CamelMicroserviceDestinationApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.microservices.camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelMicroserviceDestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelMicroserviceDestinationApplication.class, args);
+	}
+
+}
